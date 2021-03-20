fa1e2174851da34c69f1b55805110072644529fb
hiddencilantro <stevekim1109@gmail.com>
2021-03-20 16:01:58 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,8 @@ class StudentsController < ApplicationController
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
+  
 end
